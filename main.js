f3032c69b8f37b54fcb2ef88099bb591e7a016d5
Araldi <luan.araldi@gmail.com>
2016-06-03 19:54:44 -0300

    1

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ function createWindow () {
   mainWindow.loadURL(`file://${__dirname}/index.html`)
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools()
+  //mainWindow.webContents.openDevTools()
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function () {
@@ -51,3 +51,4 @@ app.on('activate', function () {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
+require('bootstrap')
