425698cb411405cf14d71e8cea8d90f274621696
Araldi <luan.araldi@gmail.com>
2016-06-28 13:04:02 -0300

    REsolvido tudo

--- js/reports.js
+++ js/reports.js
@@ -5,12 +5,13 @@ function relGastoMes(){
   var cliente = new pg.Client(conString);
   var colunas = [{title: "Mês", dataKey: "mes"},
                  {title: "Ano", dataKey: "ano"},
+                 {title: "Problemas", dataKey: "prob"},
                  {title: "Custo", dataKey: "custo"}
                ];
   var linhas = new Array();
 
   cliente.connect(function(err){
-    var query = "SELECT to_char(data,'Mon') AS Mes, extract(year from data) AS Ano,sum(custo) AS Custo FROM acao group by 1,2"
+    var query = "SELECT to_char(data_abertura,'Mon') AS Mes, extract(year from data_abertura) AS Ano,count(inconformidade.id) as prob,(SELECT SUM(custo) FROM acao,inconformidade WHERE acao.id_inconformidade = inconformidade.id) as Custo FROM inconformidade group by 1,2;"
     cliente.query(query, function(err, result) {
     });
     resultQuery = cliente.query(query);
@@ -21,8 +22,9 @@ function relGastoMes(){
       cliente.end();
       for(i = 0; i < result.rows.length; i++){
         var linhaObj = {mes:result.rows[i].mes,
-                        ano:result.rows[i].ano ,
-                        custo:result.rows[i].custo};
+                        ano:result.rows[i].ano,
+                        prob:result.rows[i].prob,
+                        custo:"R$ " + (result.rows[i].custo).toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, '$1,')};
         linhas.push(linhaObj);
         delete linhaObj;
         console.log(result.rows);
@@ -38,12 +40,13 @@ function relGastoMes(){
 function relGastoCat(){
   var cliente = new pg.Client(conString);
   var colunas = [{title: "Categoria", dataKey: "categoria"},
-                 {title: "Custo", dataKey: "custo"}
+                 {title: "Custo", dataKey: "custo"},
+                 {title: "Média Dias Resolução", dataKey: "mediaDias"}
                ];
   var linhas = new Array();
 
   cliente.connect(function(err){
-    var query = "select nome as categoria, sum(custo) as custo from acao INNER JOIN inconformidade ON(id_inconformidade = inconformidade.id) INNER JOIN categoria ON(id_categoria = categoria.id) group by 1"
+    var query = "select categoria.id as id_categoria,nome as categoria, sum(custo) as custo,sum(data_fechamento - data_abertura)/count(categoria.id) as datas from acao INNER JOIN inconformidade ON(id_inconformidade = inconformidade.id) INNER JOIN categoria ON(id_categoria = categoria.id) group by 1;"
 
     cliente.query(query, function(err, result) {
     });
@@ -55,7 +58,9 @@ function relGastoCat(){
       cliente.end();
       for(i = 0; i < result.rows.length; i++){
         var linhaObj = {categoria:result.rows[i].categoria ,
-                        custo:result.rows[i].custo};
+                        custo:"R$" + (result.rows[i].custo).toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, '$1,'),
+                        mediaDias:result.rows[i].datas
+                      };
         linhas.push(linhaObj);
         delete linhaObj;
         console.log(result.rows);
